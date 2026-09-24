80c42b2ad4c54d6d9a9dc1e4f0ea69f1cb417970
briancorn <datngo2005@gmail.com>
2026-09-24 08:54:48 +0700

    Đổi lại màu map

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -73,7 +73,7 @@ const MapComponent = ({
     >
       {/* Đã trả lại Base Map nền tối để không bị lỗi xám bản đồ */}
       <TileLayer
-        url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
         attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
       />
 
